5fe487ad08725287f1a6527f370542e8e117c35c
Haitham Alathamneh <alathamneh.haitham@gmail.com>
2023-10-22 22:54:27 +0200

    add manual reload button

--- src/components/MyFeed/UserFeedArticles.tsx
+++ src/components/MyFeed/UserFeedArticles.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useAuth } from '@/hooks/auth';
 import { useArticles } from '@/hooks/articles';
 import { ArticlesList } from '@/components/NewsList/ArticlesList';
-import { IconAdjustmentsFilled } from '@tabler/icons-react';
+import { IconAdjustmentsFilled, IconReload } from '@tabler/icons-react';
 import { Button } from 'antd';
 import { useRouter } from 'next/router';
 
@@ -22,18 +22,21 @@ export const UserFeedArticles = () => {
   useEffect(() => {
     if (!user?.settings) return;
     reload();
-  }, [user?.settings]);
+  }, [user?.settings, reload]);
 
   return (
     <>
       <div className="flex justify-between mt-4 pb-6 border-b border-slate-200">
         <h2 className="text-2xl font-bold">Your feed</h2>
-        <Button
-          type="primary"
-          icon={<IconAdjustmentsFilled className="w-5" />}
-          onClick={() => router.push('/profile?tab=feed')}>
-          Customize your feed
-        </Button>
+        <div className="flex gap-4 items-center">
+          <Button icon={<IconReload className="w-5" />} onClick={reload} />
+          <Button
+            type="primary"
+            icon={<IconAdjustmentsFilled className="w-5" />}
+            onClick={() => router.push('/profile?tab=feed')}>
+            <span className="!hidden lg:!inline">Customize your feed</span>
+          </Button>
+        </div>
       </div>
       <ArticlesList
         articles={articles}
